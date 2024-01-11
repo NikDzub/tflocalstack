77fbe62f63c714c239659bc9c83b957faeb8a7d8
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-01-12 01:20:52 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## .
+
+https://github.com/antonbabenko/terraform-best-practices
+https://docs.localstack.cloud/user-guide/integrations/terraform/
+https://registry.terraform.io/browse/providers
---
+++ tf_prjct/modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "pub_sub_az1_cidr" {}
+variable "pub_sub_az2_cidr" {}
+variable "private_app_sub_az1_cidr" {}
+variable "private_app_sub_az2_cidr" {}
