220251060af08d7c653b7d2775b53b6239d48b2c
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-01-12 10:01:31 +0200

    .

--- environments/development/terraform.tfvars
+++ environments/development/terraform.tfvars
@@ -5,8 +5,8 @@ vpc_cidr="10.0.0.0/16"
 public_sub_az1_cidr="10.0.0.0/24"
 public_sub_az2_cidr="10.0.1.0/24"
 
-private_app_sub_az1_cidr = "10.0.100.0/24"
-private_app_sub_az2_cidr = "10.0.101.0/24"
+private_app_sub_az1_cidr = "10.0.2.0/24"
+private_app_sub_az2_cidr = "10.0.3.0/24"
 
-private_data_sub_az1_cidr = "10.0.110.0/24"
-private_data_sub_az2_cidr = "10.0.111.0/24"
+private_data_sub_az1_cidr = "10.0.4.0/24"
+private_data_sub_az2_cidr = "10.0.5.0/24"
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -71,7 +71,7 @@ resource "aws_route_table_association" "public_subnet_az1_route_table_associatio
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id      = aws_subnet.public_subnet_az2
+  subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
@@ -114,7 +114,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
 # create private data subnet az2
 resource "aws_subnet" "private_data_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_data_sub_az1_cidr
+  cidr_block              = var.private_data_sub_az2_cidr
   availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
