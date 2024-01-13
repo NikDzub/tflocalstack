ce7362ecdc2d3dc911a454f8c16291e00e353b09
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-01-13 22:11:59 +0200

    .

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.project_name}_igw"
   }
 }
-##############################################
+################## subnets ######################
 resource "aws_subnet" "pub_sub" {
   count = var.az_count * var.pub_sub_per_az_count
 
@@ -24,7 +24,6 @@ resource "aws_subnet" "pub_sub" {
   cidr_block              = element(var.pub_sub_cidr_blocks, count.index)
   availability_zone       = element(data.aws_availability_zones.az.names, count.index % var.az_count)
   map_public_ip_on_launch = true
-
   tags = {
     Name = "public-subnet-${count.index + 1}"
   }
@@ -35,11 +34,31 @@ resource "aws_subnet" "priv_sub" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.priv_sub_cidr_blocks, count.index)
   availability_zone = element(data.aws_availability_zones.az.names, count.index % var.az_count)
-  #   map_public_ip_on_launch = true
-
-
   tags = {
     Name = "private-subnet-${count.index + 1}"
   }
 }
-##############################################
+################## route table #####################
+resource "aws_route_table" "pub_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "public route table" }
+}
+resource "aws_route_table" "priv_rt" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_route_table_association" "pub_sub_asso" {
+  count          = length(aws_subnet.pub_sub)
+  subnet_id      = aws_subnet.pub_sub[count.index].id
+  route_table_id = aws_route_table.pub_rt.id
+}
+resource "aws_route_table_association" "priv_sub_asso" {
+  count          = length(aws_subnet.priv_sub)
+  subnet_id      = aws_subnet.priv_rt[count.index].id
+  route_table_id = aws_route_table.priv_rt
+}
+
+
