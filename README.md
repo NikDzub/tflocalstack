221107a4e6f91cbbdc65078074e53cb46291e91e
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-01-14 04:09:52 +0200

    .

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 `https://docs.localstack.cloud/user-guide/integrations/terraform/`  
 `https://registry.terraform.io/browse/providers`  
 `https://spacelift.io/blog/terraform-aws-vpc`  
+`https://chat.openai.com/share/aa2511b5-6af5-4d25-8312-d0fe08fdd144`  
 `awslocal ec2 describe-nat-gateways`  
 `awslocal ec2 describe-vpcs`  
 `terraform apply -var-file=./env_vars/dev.tfvars -auto-approve`  
--- main.tf
+++ main.tf
@@ -6,3 +6,43 @@ module "vpc" {
   pub_sub_per_az_count  = var.pub_sub_per_az_count
   priv_sub_per_az_count = var.priv_sub_per_az_count
 }
+
+module "security_groups" {
+  source = "./modules/security_group"
+
+  security_groups = [
+    {
+      name        = "alb_sg"
+      description = "alb_sg"
+      vpc_id      = module.vpc.vpc_id
+      ingress_rules = [
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+          from_port   = 443
+          to_port     = 443
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+      ]
+    },
+    {
+      name        = "ssh_sg"
+      description = "ssh_sg"
+      vpc_id      = module.vpc.vpc_id
+      ingress_rules = [
+        {
+          from_port   = 22
+          to_port     = 22
+          protocol    = "tcp"
+          cidr_blocks = ["10.0.0.0/16"] # my_ip..
+          # security_groups = []
+        },
+      ]
+    },
+  ]
+}
